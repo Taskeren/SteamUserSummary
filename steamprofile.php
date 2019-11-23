97b74d8da18dabbeba829644baded4f741e10e6c
Taskeren <nitu2003@126.com>
2019-11-23 12:30:01 +0800

    I want more 56s!

--- steamprofile.php
+++ steamprofile.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 header('Content-Type: application/json');
 
 $url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=8ECB5619637B397611CBE9E3A7223D1C&steamids=";
